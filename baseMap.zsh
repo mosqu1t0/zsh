4af6801bd46bba19d466892b6aba32b8d4c36840
mosqu1t0 <mosquito@email.cn>
2022-05-19 08:34:49 +0800

    add map and change mod

--- baseMap.zsh
+++ baseMap.zsh
@@ -1,6 +1,7 @@
 export EDITOR='nvim'
-export HOME='/home/mosquito/'
+export HOME='/home/mosquito'
 export VIMPATH="$HOME/.config/nvim"
+export PATH="$PATH:/home/mosquito/Mybin"
 
 alias vim='nvim'
 alias ls='ls --color'
@@ -8,3 +9,5 @@ alias ll='ls -l'
 
 alias reboot='sudo systemctl reboot'
 alias poweroff='sudo systemctl poweroff'
+
+alias droptrash='sudo rm -rf /home/mosquito/.local/share/Trash/*'
