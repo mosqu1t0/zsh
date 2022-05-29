69da16d143cbd61243dd732ac9071492eaa5767d
mosqu1t0 <mosquito@email.cn>
2022-05-29 22:13:50 +0800

    color

--- baseMap.zsh
+++ baseMap.zsh
@@ -3,12 +3,28 @@ export HOME='/home/mosquito'
 export VIMPATH="$HOME/.config/nvim"
 export PATH="$PATH:/home/mosquito/Mybin"
 
+
+
+# system design ------------------------------
+
+
 alias vim='nvim'
+alias grep='grep --color=auto'
+alias diff='diff --color=auto'
+alias ip='ip -color=auto'
+export LESS='-R --use-color -Dd+r$Du+b'
 alias ls='ls --color'
 alias ll='ls -l'
+
+
+
+
+
+# user design -------------------------------
 
 alias reboot='signOut && sudo systemctl reboot'
 alias poweroff='signOut && sudo systemctl poweroff'
 
 alias droptrash='sudo rm -vrf /home/mosquito/.local/share/Trash/*'
 alias pc='proxychains4'
+
