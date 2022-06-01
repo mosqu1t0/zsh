db2be3ce04e8403431ab847706fa3e4fe087c158
mosqu1t0 <mosquito@email.cn>
2022-06-01 22:34:57 +0800

    fzf

---
+++ fzf-scope.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+case "$1" in
+	*.pdf) pdftotext "$1" ;;
+	*.md) pandoc -s -t markdown -- "$1" ;;
+	*.zip) zipinfo "$1" ;;
+	*.7z) 7z l "$1" ;;
+	*) bat --color=always --italic-text=always --style=numbers,changes,header --line-range :500 "$1" ;;
+esac
--- fzf.zsh
+++ fzf.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_OPTS="--height 60% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
+export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border --preview '~/zsh/fzf-scope.sh {} '"
 export FZF_DEFAULT_COMMAND="fd --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.local,.steam,.m2,.rangerdir,.ssh,.ghidra,.mozilla} --type f --hidden --follow"
 
 
