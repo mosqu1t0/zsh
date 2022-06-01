9a1a07b2b89269de92c0aa23510cc203315c6343
mosqu1t0 <mosquito@email.cn>
2022-06-01 22:38:30 +0800

    add fzf ekv

--- fzf.zsh
+++ fzf.zsh
@@ -1,3 +1,5 @@
+export FZF_FZM_OPTS="--reverse --height 75% --min-height 30 --cycle +m --ansi --bind=ctrl-o:accept,ctrl-t:toggle --select-1"
+export FZF_DMARK_OPTS="--reverse --height 75% --min-height 30 --cycle -m --ansi --bind=ctrl-o:accept,ctrl-t:toggle"
 export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border --preview '~/zsh/fzf-scope.sh {} '"
 export FZF_DEFAULT_COMMAND="fd --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.local,.steam,.m2,.rangerdir,.ssh,.ghidra,.mozilla} --type f --hidden --follow"
 
