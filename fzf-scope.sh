fcd4aaf6bf813c509cf704cdcc6faf7138559fe0
mosqu1t0 <mosquito@email.cn>
2022-06-04 17:50:51 +0800

    x

--- fzf-scope.sh
+++ fzf-scope.sh
@@ -5,5 +5,5 @@ case "$1" in
 	*.md) pandoc -s -t markdown -- "$1" ;;
 	*.zip) zipinfo "$1" ;;
 	*.7z) 7z l "$1" ;;
-	*) bat --color=always --italic-text=always --theme="gruvbox-dark" --style=numbers,changes,header --line-range :500 "$1" ;;
+	*.*) bat --color=always --italic-text=always --theme="gruvbox-dark" --style=numbers,changes,header --line-range :500 "$1" ;;
 esac
