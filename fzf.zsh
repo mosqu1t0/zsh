0b594d3b3054cd5e812f6d601c43f6a550cd4b49
mosqu1t0 <mosquito@email.cn>
2022-06-12 11:58:10 +0800

    fix right

--- fzf.zsh
+++ fzf.zsh
@@ -1,6 +1,7 @@
 export FZF_FZM_OPTS="--reverse --height 75% --min-height 30 --cycle +m --ansi --bind=ctrl-o:accept,ctrl-t:toggle --select-1"
 export FZF_DMARK_OPTS="--reverse --height 75% --min-height 30 --cycle -m --ansi --bind=ctrl-o:accept,ctrl-t:toggle"
-export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border --preview '~/zsh/fzf-scope.sh {} '"
+# export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border --preview '~/zsh/fzf-scope.sh {} '"
+export FZF_DEFAULT_OPTS="--height 80% --layout=reverse --border "
 export FZF_DEFAULT_COMMAND="fd --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.local,.steam,.m2,.rangerdir,.ssh,.ghidra,.mozilla} --type f --hidden --follow"
 
 
@@ -9,7 +10,7 @@ export FZF_DEFAULT_COMMAND="fd --exclude={.wine,.git,.idea,.vscode,.sass-cache,n
 # fkill - kill process
 fkill() {
   local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  pid=$(ps -ef | sed 1d | fzf -m --keep-right | awk '{print $2}')
 
   if [ "x$pid" != "x" ]
   then
